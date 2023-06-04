531f86ee223c2624031e507961cb5d23bf8eac8d
Saron <saronbrandao@gmail.com>
2023-06-04 13:49:37 +0200

    updates

--- src/js/model.js
+++ src/js/model.js
@@ -70,8 +70,10 @@ export const loadSearchResults = async function (query) {
   try {
     state.search.query = query;
 
-    const data = await AJAX(`${API_URL}?search=${query}&key=${API_KEY}`);
-    //console.log(data);
+    const data = await AJAX(`${API_URL}?search=${query}`);
+
+    // Get request with api key is not necessary in this demo
+    // const data = await AJAX(`${API_URL}?search=${query}&key=${API_KEY}`);
 
     state.search.results = data.data.recipes.map((rec) => {
       return {
